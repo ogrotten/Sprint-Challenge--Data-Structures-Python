fa817cdfbf3ce09dd659a59986f37d5da79a94c6
chip <chiposaur@gmail.com>
2020-03-27 13:53:21 -0500

    retooled ring into pseudo

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,30 +9,28 @@ class RingBuffer:
 		self.size = 0
 
     def append(self, item):
-		if self.size = 0
-			# add to the head.
-			pass
+		# if size == cap
+		# 	then overwrite oldest which is head
+		# 	then increment current to next
 
-		if self.size < self.capacity:
-			# add to the tail.
-			pass
-
-		if self.current == self.storage.head
-			# 
-			pass
-
-		if selft.curren == self.storage.tail
-			pass
-
-		# if none of that, then 
-
+		# else if size < cap
+		# 	then add to tail
+		# 	but NOW...
+		# 	if size = cap
+		#		then tail has no .next
+		# 		then set tail.next to head
+		#
 
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+		# view = head
+		# put the view contents in LBC
+		# while view.next isn't head
+		# 	put view.next contents in LBC
+		#	view = view.next
 
         return list_buffer_contents
 
